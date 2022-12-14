ebc773cf1d62d182b8a7739068a505fb7b7a3356
Mayhem Studios <55484272+MayhemStudios@users.noreply.github.com>
2022-12-15 01:28:26 +1000

    Update fxmanifest.lua

--- MayhemTattoosStandalone/fxmanifest.lua
+++ MayhemTattoosStandalone/fxmanifest.lua
@@ -1,5 +1,6 @@
 fx_version 'cerulean'
 author 'MayhemStudios'
+games 'gta5'
 discription 'Mayhem Studios Tattoo Pack'
 discord 'https://discord.gg/b9upz9NkwC'
 repository 'https://github.com/MayhemStudios/MayhemTattoos'
