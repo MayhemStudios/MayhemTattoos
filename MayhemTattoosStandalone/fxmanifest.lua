460030c95e521ad8ed945a35686f4bf4aed5b3e5
Mayhem Studios <55484272+MayhemStudios@users.noreply.github.com>
2022-12-14 06:54:55 -0800

    Add files via upload

---
+++ MayhemTattoosStandalone/fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'cerulean'
+author 'MayhemStudios'
+discription 'Mayhem Studios Tattoo Pack'
+discord 'https://discord.gg/b9upz9NkwC'
+repository 'https://github.com/MayhemStudios/MayhemTattoos'
+version '1.0.0'
+
+
+files {
+    'tattoos_overlay.xml',
+}
+
+
+data_file 'PED_OVERLAY_FILE' 'tattoos_overlay.xml'
